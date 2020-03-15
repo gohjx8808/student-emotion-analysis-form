7c24f10ffb906970aa0535d8564f23e48df0a8e3
gohjx8808 <jingxuan.goh@capitalbay.com.my>
2020-03-15 19:42:37 +0800

    initial commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Kreait\Firebase;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+
+class TestController extends Controller
+{
+    //
+    public function index()
+    {
+        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__ . '/student-emotion-analysis-d61dc-3e42dda5c936.json');
+        $firebase = (new Factory)
+            ->withServiceAccount($serviceAccount)
+            ->withDatabaseUri('https://student-emotion-analysis-d61dc.firebaseio.com/')
+            ->create();
+
+        $database = $firebase->getDatabase();
+
+        $newPost = $database
+            ->getReference('blog/posts')
+            ->push([
+                'title' => 'Laravel FireBase Tutorial',
+                'category' => 'Laravel'
+            ]);
+        echo '<pre>';
+        print_r($newPost->getvalue());
+    }
+}
